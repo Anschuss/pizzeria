97a240d1accc95dec389087bd5676829b2a12330
alexey <cuteracoon@gmail.com>
2021-03-12 15:43:04 +0600

    added new model for app menu (CartFood, Cart and Customer)

--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,13 @@
 import hashlib
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
+User = get_user_model()
+
+class Types(models.Model):
+    pass
 
 class CompositionDish(models.Model):
     text = models.TextField()
@@ -49,3 +56,39 @@ class Sauces(Product):
 
     def __str__(self):
         return f"{self.name}, {self.price}"
+
+
+## Order models
+
+class CartFood(models.Model):
+    # user = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='related_food')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_objcet = GenericForeignKey('content_type', 'object_id')
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return f"Чек:{self.content_objcet}"
+
+
+class Cart(models.Model):
+    # owner = models.ForeignKey('Customer', null=True, on_delete=models.CASCADE)
+    food = models.ManyToManyField(CartFood, blank=True, related_name='related_cart')
+    total_food = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2)
+    in_order = models.BooleanField(default=False)
+    for_anomymous_user = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
